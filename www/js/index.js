025f40e3bcb151fc1e5d3e7841f92bb944e14c15
soruly <soruly@gmail.com>
2021-03-29 18:06:06 +0800

    Update 2021-04

--- www/js/index.js
+++ www/js/index.js
@@ -1267,7 +1267,7 @@ window.getListing = async (scroll) => {
       window.location.pathname === "/OVA/" ||
       window.location.pathname === "/Sukebei/" ||
       window.location.pathname === "/2021-01/" ||
-      window.location.pathname === "/2020-10/"
+      window.location.pathname === "/2021-04/"
     ) {
       dirEntries.sort((a, b) => (b.modified > a.modified ? 1 : b.modified < a.modified ? -1 : 0));
     } else if (window.location.pathname === "/") {
