d0a35ac70171ba0242b32747f841b55a546f187f
soruly <soruly@gmail.com>
2021-09-24 19:04:45 +0800

    Add watch and release build settings

--- script/build.js
+++ script/build.js
@@ -3,17 +3,33 @@ import esbuild from "esbuild";
 esbuild.build({
   entryPoints: ["./www/js/mobile.js"],
   outfile: "./www/js/mobile.es2016.js",
-  minify: false,
+  minify: process.argv.includes("--release"),
   bundle: true,
-  sourcemap: false,
+  sourcemap: true,
   target: ["es2016"],
+  watch: !process.argv.includes("--watch")
+    ? null
+    : {
+        onRebuild(error, result) {
+          if (error) console.error("watch build failed:", error);
+          else console.log("watch build succeeded:", result);
+        },
+      },
 });
 
 esbuild.build({
   entryPoints: ["./www/js/desktop.js"],
   outfile: "./www/js/desktop.es2016.js",
-  minify: false,
+  minify: process.argv.includes("--release"),
   bundle: true,
-  sourcemap: false,
+  sourcemap: true,
   target: ["es2016"],
+  watch: !process.argv.includes("--watch")
+    ? null
+    : {
+        onRebuild(error, result) {
+          if (error) console.error("watch build failed:", error);
+          else console.log("watch build succeeded:", result);
+        },
+      },
 });
