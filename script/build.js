f862fa0f7f7a8d56ca051694996b8da9a5a9799f
soruly <soruly@gmail.com>
2021-09-25 20:07:12 +0800

    Split css into modules

--- script/build.js
+++ script/build.js
@@ -18,6 +18,24 @@ esbuild.build({
 });
 
 esbuild.build({
+  entryPoints: ["./src/App.css"],
+  outfile: "./www/css/mobile.min.css",
+  minify: process.argv.includes("--release"),
+  bundle: true,
+  sourcemap: true,
+  loader: { ".jpg": "file" },
+  target: ["chrome58", "firefox57", "safari11"],
+  watch: !process.argv.includes("--watch")
+    ? null
+    : {
+        onRebuild(error, result) {
+          if (error) console.error("watch build failed:", error);
+          else console.log("watch build succeeded:", result);
+        },
+      },
+});
+
+esbuild.build({
   entryPoints: ["./www/js/desktop.js"],
   outfile: "./www/js/desktop.es2016.js",
   minify: process.argv.includes("--release"),
